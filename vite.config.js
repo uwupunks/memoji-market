ef2d83f08e052f37b42eb5d5c468a9a6151172af
smoothiepool <smoothiepool@uwublack.market>
2024-08-17 17:14:35 -0500

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-polyfill-node";
-import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
-
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   assetsInclude: ["**/*.vox"],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Enable esbuild polyfill plugins
-      plugins: [nodeModulesPolyfillPlugin()],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [nodePolyfills()],
-    },
-  },
 });

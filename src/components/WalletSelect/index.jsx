35ce33af15022919bbdf9b013a1ee2007e503e19
smoothiepool <smoothiepool@uwublack.market>
2024-08-23 20:11:29 -0500

    Firefox suggest chain for keplr

--- src/components/WalletSelect/index.jsx
+++ src/components/WalletSelect/index.jsx
@@ -1,95 +1,153 @@
-
 import useSound from "use-sound";
 
 import clickMp3 from "assets/sounds/btclick.mp3";
 import promptMp3 from "assets/sounds/prompt.mp3";
-import { Modal } from 'react-responsive-modal'
-import 'react-responsive-modal/styles.css';
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 
 import overMp3 from "assets/sounds/btmouseover.mp3";
 
-import { throttle } from 'lodash';
+import { throttle } from "lodash";
 
-import "./index.css"
+import "./index.css";
+
+const suggest = async () => {
+  const chainInfo = {
+    chainId: "unicorn-420",
+    chainName: "unicorn",
+    rest: "https://rest.unicorn.meme",
+    rpc: "https://rpc.unicorn.meme",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "unicorn",
+      bech32PrefixAccPub: "unicorn" + "pub",
+      bech32PrefixValAddr: "unicorn" + "valoper",
+      bech32PrefixValPub: "unicorn" + "valoperpub",
+      bech32PrefixConsAddr: "unicorn" + "valcons",
+      bech32PrefixConsPub: "unicorn" + "valconspub",
+    },
+    currencies: [
+      {
+        coinDenom: "UWU",
+        coinMinimalDenom: "uwunicorn",
+        coinDecimals: 6,
+        coinImageUrl:
+          "https://bafkreid2zugjmwborka6qt4mxlb3vepnnxj344dtyaf4gmdsogzd2dh2a4.ipfs.w3s.link",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "🦄",
+        coinMinimalDenom: "uwunicorn",
+        coinDecimals: 6,
+        gasPriceStep: {
+          low: 0.01,
+          average: 0.025,
+          high: 0.04,
+        },
+      },
+    ],
+    stakeCurrency: {
+      coinDenom: "🦄",
+      coinMinimalDenom: "uwunicorn",
+      coinDecimals: 6,
+    },
+    features: ["cosmwasm"],
+  };
+
+  if (window.keplr) {
+    await window.keplr.experimentalSuggestChain(chainInfo);
+  }
+  if (window.leap) {
+    await window.leap.experimentalSuggestChain(chainInfo);
+  }
+
+  return res;
+};
 
 function WalletSelect({ isOpen, setOpen, walletRepo }) {
-    //hooks
-    const [clickSound] = useSound(clickMp3);
-    const [overSound] = useSound(overMp3);
-    const [promptSound] = useSound(promptMp3);
+  //hooks
+  const [clickSound] = useSound(clickMp3);
+  const [overSound] = useSound(overMp3);
+  const [promptSound] = useSound(promptMp3);
 
-    const playOverSound = throttle(overSound, 100)
+  const playOverSound = throttle(overSound, 100);
 
-    return isOpen ? <Modal
-        open={isOpen}
-        onClose={() => setOpen(false)}
-        center
-        showCloseIcon={false}
-        classNames={{
-            overlay: 'walletSelectOverlay',
-            modal: 'walletSelectModal',
-        }}
-
+  return isOpen ? (
+    <Modal
+      open={isOpen}
+      onClose={() => setOpen(false)}
+      center
+      showCloseIcon={false}
+      classNames={{
+        overlay: "walletSelectOverlay",
+        modal: "walletSelectModal",
+      }}
     >
-
-        <div className="header" onMouseDown={() => clickSound()}></div>
-        {walletRepo?.wallets.map(({ walletName, connect }) => (
-
-            <div key={walletName} className={'option ' + walletName}
-                onClick={async () => {
-                    promptSound()
-                    try {
-                    await connect()
-                    } catch (err) {
-                        window.alert(err)
-                        console.error(err)
-                    }
-                    setOpen(false)
-                }}
-                onMouseEnter={() => playOverSound()}
-                onMouseDown={() => clickSound()}>
-                <div className="icon-bg"><div className="icon"></div></div>
-                <div className="name">
-                    <div className="textbox">
-                        <div className={walletName}></div>
-                    </div>
-                    <button
-                    >
-                    </button>
-                </div>
-                <div className="spacer"></div>
+      <div className="header" onMouseDown={() => clickSound()}></div>
+      {walletRepo?.wallets.map(({ walletName, connect }) => (
+        <div
+          key={walletName}
+          className={"option " + walletName}
+          onClick={async () => {
+            promptSound();
+            try {
+              const res = await suggest();
+              debugger;
+              await connect();
+            } catch (err) {
+              window.alert(err);
+              console.error(err);
+            }
+            setOpen(false);
+          }}
+          onMouseEnter={() => playOverSound()}
+          onMouseDown={() => clickSound()}
+        >
+          <div className="icon-bg">
+            <div className="icon"></div>
+          </div>
+          <div className="name">
+            <div className="textbox">
+              <div className={walletName}></div>
             </div>
-        ))
-        }
-        <div className="option disabled" onMouseDown={() => clickSound()}>
-            <div className="icon-bg"><div className="icon"></div></div>
-            <div className="name">
-                <div className="textbox">
-                </div>
-                <button>
-                </button>
-            </div>
-            <div className="spacer"></div>
+            <button></button>
+          </div>
+          <div className="spacer"></div>
         </div>
-        <div className="option disabled" onMouseDown={() => clickSound()}>
-            <div className="icon-bg"><div className="icon"></div></div>
-            <div className="name">
-                <div className="textbox">
-                </div>
-                <button>
-                </button>
-            </div>
+      ))}
+      <div className="option disabled" onMouseDown={() => clickSound()}>
+        <div className="icon-bg">
+          <div className="icon"></div>
         </div>
-        <div className="option disabled" onMouseDown={() => clickSound()}>
-            <div className="icon-bg"><div className="icon"></div></div>
-            <div className="name">
-                <div className="textbox">
-                </div>
-                <button>
-                </button>
-            </div>
+        <div className="name">
+          <div className="textbox"></div>
+          <button></button>
         </div>
-    </Modal > : null
+        <div className="spacer"></div>
+      </div>
+      <div className="option disabled" onMouseDown={() => clickSound()}>
+        <div className="icon-bg">
+          <div className="icon"></div>
+        </div>
+        <div className="name">
+          <div className="textbox"></div>
+          <button></button>
+        </div>
+      </div>
+      <div className="option disabled" onMouseDown={() => clickSound()}>
+        <div className="icon-bg">
+          <div className="icon"></div>
+        </div>
+        <div className="name">
+          <div className="textbox"></div>
+          <button></button>
+        </div>
+      </div>
+    </Modal>
+  ) : null;
 }
 
 export { WalletSelect };

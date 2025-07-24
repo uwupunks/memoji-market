4eeb5c1076fa464a1b827584054d806ec8a5d725
smoothiepool <smoothiepool@uwublack.market>
2025-07-23 22:57:18 -0500

    feat: yami time

--- src/WinXP/apps/Winamp/config.jsx
+++ src/WinXP/apps/Winamp/config.jsx
@@ -1,83 +1,30 @@
 // Auto-generated imports for all uwu music files
-import FriedNoodles from "assets/music/uwu/02 Fried Noodles _DVNZXL Remix_ - Pink Season Extras - SoundLoadMate.com.mp3";
-import AlinaBasshunter from "assets/music/uwu/Alina _ Basshunter - When You Leave _M.P.E. Remix_ _FREE DOWNLOAD_ - M.P.E. - SoundLoadMate.com.mp3";
-import BakemonogatariSutekiMeppou from "assets/music/uwu/Bakemonogatari Suteki Meppou - RosesAreRed - SoundLoadMate.com.mp3";
-import BarreBaby from "assets/music/uwu/Barre Baby _feat. Ronnetta Spencer_ - Big Moe - SoundLoadMate.com.mp3";
-import GangnamStyleNightcore from "assets/music/uwu/Gangnam Style Nightcore. - IDoNightcore - SoundLoadMate.com.mp3";
-import Instasamka from "assets/music/uwu/INSTASAMKA _ Пампим нефть _ speed up _ - shwtyhub - SoundLoadMate.com.mp3";
 import NightcoreEverytimeWeTouch from "assets/music/uwu/Nightcore Everytime We Touch - SuperstarElf - SoundLoadMate.com.mp3";
-import NoRules from "assets/music/uwu/No Rules_ - Windows95Man - SoundLoadMate.com.mp3";
 import PlatinumDisco from "assets/music/uwu/Platinum Disco _ Eurobeat Remix - Turbo - SoundLoadMate.com.mp3";
-import PursuingMyTrueSelf from "assets/music/uwu/Pursuing My True Self - xayusu - SoundLoadMate.com.mp3";
 import SchnuffelHaschenparty from "assets/music/uwu/schnuffel - häschenparty _ bunny party _sped up_ - лимон - SoundLoadMate.com.mp3";
 import DuvetBoa from "assets/music/uwu/𝗗𝘂𝘃𝗲𝘁 𝗕𝗼𝗮 - 𝗦𝗽𝗲𝗱 𝘂𝗽 - gore poop - SoundLoadMate.com.mp3";
+import TheSynarchy from "assets/music/uwu/The Synarchy - Yami - SoundLoadMate.com.mp3";
+import NoRules from "assets/music/uwu/No Rules_ - Windows95Man - SoundLoadMate.com.mp3";
+import BunnyGirlSenpaiDrillRemix from "assets/music/uwu/Bunny Girl Senpai Drill Remix - Baba - SoundLoadMate.com.mp3";
+import INSTASAMKA from "assets/music/uwu/INSTASAMKA _ Пампим нефть _ speed up _ - shwtyhub - SoundLoadMate.com.mp3";
+import HalogenDreams from "assets/music/uwu/Halogen Dreams - Yamí __ - SoundLoadMate.com.mp3";
+import GangnamStyleNightcore from "assets/music/uwu/Gangnam Style Nightcore. - IDoNightcore - SoundLoadMate.com.mp3";
+import BakemonogatariSutekiMeppou from "assets/music/uwu/Bakemonogatari Suteki Meppou - RosesAreRed - SoundLoadMate.com.mp3";
+import GoldenTimes from "assets/music/uwu/Golden Times - Yamí __ - SoundLoadMate.com.mp3";
+import BarreBaby from "assets/music/uwu/Barre Baby _feat. Ronnetta Spencer_ - Big Moe - SoundLoadMate.com.mp3";
+import AlinaBasshunter from "assets/music/uwu/Alina _ Basshunter - When You Leave _M.P.E. Remix_ _FREE DOWNLOAD_ - M.P.E. - SoundLoadMate.com.mp3";
+import FallSlater from "assets/music/uwu/Fall - Slater - SoundLoadMate.com.mp3";
+import FriedNoodles from "assets/music/uwu/02 Fried Noodles _DVNZXL Remix_ - Pink Season Extras - SoundLoadMate.com.mp3";
+import PursuingMyTrueSelf from "assets/music/uwu/Pursuing My True Self - xayusu - SoundLoadMate.com.mp3";
 
-function shuffleArray(array) {
-  let currentIndex = array.length,
-    randomIndex;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-  return array;
-}
 
-export const initialTracks = shuffleArray([
+export const initialTracks = [
   {
-    url: FriedNoodles,
+    url: TheSynarchy,
     duration: null,
     metaData: {
-      title: "Fried Noodles (DVNZXL Remix)",
-      artist: "Pink Season Extras",
-      album: "uwu",
-    },
-  },
-  {
-    url: AlinaBasshunter,
-    duration: null,
-    metaData: {
-      title: "Alina / Basshunter - When You Leave (M.P.E. Remix)",
-      artist: "M.P.E.",
-      album: "uwu",
-    },
-  },
-  {
-    url: BakemonogatariSutekiMeppou,
-    duration: null,
-    metaData: {
-      title: "Bakemonogatari Suteki Meppou",
-      artist: "RosesAreRed",
-      album: "uwu",
-    },
-  },
-  {
-    url: BarreBaby,
-    duration: null,
-    metaData: {
-      title: "Barre Baby (feat. Ronnetta Spencer)",
-      artist: "Big Moe",
-      album: "uwu",
-    },
-  },
-  {
-    url: GangnamStyleNightcore,
-    duration: null,
-    metaData: {
-      title: "Gangnam Style Nightcore",
-      artist: "IDoNightcore",
-      album: "uwu",
-    },
-  },
-  {
-    url: Instasamka,
-    duration: null,
-    metaData: {
-      title: "INSTASAMKA (Пампим нефть speed up)",
-      artist: "shwtyhub",
+      title: "The Synarchy",
+      artist: "Yami",
       album: "uwu",
     },
   },
@@ -91,29 +38,11 @@ export const initialTracks = shuffleArray([
     },
   },
   {
-    url: NoRules,
-    duration: null,
-    metaData: {
-      title: "No Rules",
-      artist: "Windows95Man",
-      album: "uwu",
-    },
-  },
-  {
     url: PlatinumDisco,
     duration: null,
     metaData: {
       title: "Platinum Disco (Eurobeat Remix)",
       artist: "Turbo",
-      album: "uwu",
-    },
-  },
-  {
-    url: PursuingMyTrueSelf,
-    duration: null,
-    metaData: {
-      title: "Pursuing My True Self",
-      artist: "xayusu",
       album: "uwu",
     },
   },
@@ -135,4 +64,112 @@ export const initialTracks = shuffleArray([
       album: "uwu",
     },
   },
-]);
+  {
+    url: NoRules,
+    duration: null,
+    metaData: {
+      title: "No Rules",
+      artist: "Windows95Man",
+      album: "uwu",
+    },
+  },
+  {
+    url: BunnyGirlSenpaiDrillRemix,
+    duration: null,
+    metaData: {
+      title: "Bunny Girl Senpai Drill Remix",
+      artist: "Baba",
+      album: "uwu",
+    },
+  },
+  {
+    url: INSTASAMKA,
+    duration: null,
+    metaData: {
+      title: "INSTASAMKA (Пампим нефть speed up)",
+      artist: "shwtyhub",
+      album: "uwu",
+    },
+  },
+  {
+    url: HalogenDreams,
+    duration: null,
+    metaData: {
+      title: "Halogen Dreams",
+      artist: "Yamí __",
+      album: "uwu",
+    },
+  },
+  {
+    url: GangnamStyleNightcore,
+    duration: null,
+    metaData: {
+      title: "Gangnam Style Nightcore",
+      artist: "IDoNightcore",
+      album: "uwu",
+    },
+  },
+  {
+    url: BakemonogatariSutekiMeppou,
+    duration: null,
+    metaData: {
+      title: "Bakemonogatari Suteki Meppou",
+      artist: "RosesAreRed",
+      album: "uwu",
+    },
+  },
+  {
+    url: GoldenTimes,
+    duration: null,
+    metaData: {
+      title: "Golden Times",
+      artist: "Yamí __",
+      album: "uwu",
+    },
+  },
+  {
+    url: BarreBaby,
+    duration: null,
+    metaData: {
+      title: "Barre Baby (feat. Ronnetta Spencer)",
+      artist: "Big Moe",
+      album: "uwu",
+    },
+  },
+  {
+    url: AlinaBasshunter,
+    duration: null,
+    metaData: {
+      title: "Alina / Basshunter - When You Leave (M.P.E. Remix)",
+      artist: "M.P.E.",
+      album: "uwu",
+    },
+  },
+  {
+    url: FallSlater,
+    duration: null,
+    metaData: {
+      title: "Fall",
+      artist: "Slater",
+      album: "uwu",
+    },
+  },
+  {
+    url: FriedNoodles,
+    duration: null,
+    metaData: {
+      title: "02 Fried Noodles (DVNZXL Remix)",
+      artist: "Pink Season Extras",
+      album: "uwu",
+    },
+  },
+  {
+    url: PursuingMyTrueSelf,
+    duration: null,
+    metaData: {
+      title: "Pursuing My True Self",
+      artist: "xayusu",
+      album: "uwu",
+    },
+  },
+];
